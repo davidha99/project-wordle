72740195633802b35e0b213292f7f6e154df26da
David Hernandez <d.hdza19@gmail.com>
2024-06-27 17:20:28 -0600

    Exercise 3: Guess slots

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guessHistory }) {
   return (
-    <ul className="guess-results">
-      {guessHistory.map((guess, index) => (
-        <li key={index} className="guess">
-          {guess}
-        </li>
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guessHistory[num]} />
       ))}
-    </ul>
+    </div>
   );
 }
 
